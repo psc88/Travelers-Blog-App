82efce469ab2763598dd67cd10217f2de2b598c7
Pablo Castillo <psc88sdrd@gmail.com>
2023-08-29 11:50:25 -0300

    card layout and json server installation

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,51 @@
+import {
+  Card,
+  Typography,
+  CardHeader,
+  Avatar,
+  IconButton,
+  CardMedia,
+  CardContent,
+  CardActions
+} from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { red, grey } from '@mui/material/colors';
+
+export const Cards = () => {
+
+  const colorRed = red[500];
+  const colorGrey = grey[900];
+
+  return (
+    <Card sx={{ bgcolor: colorGrey, margin: 1 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: colorRed }} aria-label="recipe">
+            P
+          </Avatar>
+        }
+        title={<Typography color="white" >Pablo Castillo</Typography>}
+        subheader={<Typography color="white" >Agosto 29, 2023</Typography>}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="https://img.freepik.com/fotos-premium/campo-flores-arbol-primer-plano_865967-7108.jpg"
+        alt="Paisaje"
+      />
+      <CardContent>
+        <Typography variant="body2" color="white">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni illum est rerum? Totam neque explicabo tempora architecto eius eos inventore quia accusamus sapiente quos, numquam harum nobis iusto unde cum.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon sx={{ color: colorRed }} />
+        </IconButton>
+        <Typography variant="h6" color="white">
+          {`1500 Me gusta`}
+        </Typography>
+      </CardActions>
+    </Card>
+  )
+}
--- src/pages/HomeBlog/index.tsx
+++ src/pages/HomeBlog/index.tsx
@@ -1,7 +1,20 @@
+import { Box, Grid, Typography } from "@mui/material"
+import { Cards } from "../../components"
+
 export const HomeBlog = () => {
+
   return (
     <>
-      <h1>Soy el HomeBlog</h1>
+      <Box sx={{ flexGrow: 1 }} margin={5}>
+        <Grid container spacing={2}>
+          <Grid item lg={8} md={6} sm={12} xs={12}>
+            <Cards/>
+          </Grid>
+          <Grid item lg={4} md={6} sm={12} xs={12}>
+            <Typography className='fs-3'>xs=8</Typography>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   )
 }
